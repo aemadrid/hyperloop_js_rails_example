2d3bfaf362575c6471fab7a6d5bdc15f82d699d9
Adrian Esteban Madrid <aemadrid@gmail.com>
2017-10-10 11:58:53 -0600

    Getting an error using the gem source.

--- app/assets/javascripts/application.js.rb
+++ app/assets/javascripts/application.js.rb
@@ -1,9 +1,6 @@
 require 'vendor/react'
 require 'vendor/react-dom'
 require 'vendor/jquery-2.1.4'
-require 'vendor/history'
-require 'vendor/react-router'
-require 'vendor/react-router-dom'
 
 require 'opal'
 require 'opal-browser'
@@ -13,6 +10,6 @@ require 'active_support'
 
 require 'vendor/hyperloop'
 require 'vendor/hyperloop-compiler'
-require 'vendor/hyper-router'
+require 'hyper-router/react-router-source'
 
 
